2d41d58fc4365249bc5020f46612f2a6293ea985
ganchurin <eugeny.ganchurin@gmail.com>
2015-07-24 00:23:31 +0300

    Draft - refactoring: added support for 2-dimensional arrays

--- src/main/java/com/ganchurin/model/ArrayDataSource.java
+++ src/main/java/com/ganchurin/model/ArrayDataSource.java
@@ -2,29 +2,34 @@ package com.ganchurin.model;
 
 public class ArrayDataSource implements DataSource {
 
-	private final String[] array;
+	private final String[][] array;
 
 	public ArrayDataSource(String[] array) {
+		this.array = new String[1][];
+		this.array[0] = array;
+	}
+
+	public ArrayDataSource(String[][] array) {
 		this.array = array;
 	}
 
 	@Override
 	public String getValue(int row, int col) {
-		return array[col];
+		return array[row][col];
 	}
 
 	@Override
 	public void setValue(int row, int col, String value) {
-		array[col] = value;
+		array[row][col] = value;
 	}
 
 	@Override
 	public int getRowsCount() {
-		return 1;
+		return array.length;
 	}
 
 	@Override
 	public int getColumnsCount() {
-		return array.length;
+		return array[0].length;
 	}
 }
