78979942163fb34d518bae474168e412b5b99c35
ganchurin <eugeny.ganchurin@gmail.com>
2015-07-28 02:41:39 +0300

    Draft - refactoring: rolled some changes back

--- src/main/java/com/ganchurin/view/PrintFormat.java
+++ src/main/java/com/ganchurin/view/PrintFormat.java
@@ -2,56 +2,20 @@ package com.ganchurin.view;
 
 import com.ganchurin.model.DataSource;
 
-public abstract class PrintFormat {
+interface PrintFormat {
 
-	abstract char getCornerChar();
+	char getCornerChar();
 
-	abstract PrintFormat setCornerChar(char c);
+	PrintFormat setCornerChar(char c);
 
-	abstract char getRowChar();
+	char getRowChar();
 
-	abstract PrintFormat setRowChar(char c);
+	PrintFormat setRowChar(char c);
 
-	abstract char getColumnChar();
+	char getColumnChar();
 
-	abstract PrintFormat setColumnChar(char c);
+	PrintFormat setColumnChar(char c);
 
-	public String format(DataSource source) {
-		StringBuilder sb = new StringBuilder();
-
-		int rows = source.getRowsCount() * 2 + 1;
-		int cols = source.getColumnsCount() * 2 + 1;
-
-		for (int row = 0; row < rows; row++) {
-			if (!isDataRow(row)) {
-				for (int j = 0; j < cols; j++) {
-					if (!isDataColumn(j)) {
-						sb.append(getCornerChar());
-					} else {
-						sb.append(getRowChar());
-					}
-				}
-				sb.append("\n");
-				continue;
-			}
-			for (int col = 0; col < cols; col++) {
-				if (isDataColumn(col)) {
-					sb.append(source.getValue(row / 2, col / 2));
-				} else {
-					sb.append(getColumnChar());
-				}
-			}
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
-
-	private boolean isDataRow(int row) {
-		return row % 2 == 1;
-	}
-
-	private boolean isDataColumn(int col) {
-		return col % 2 == 1;
-	}
+	String format(DataSource source);
 
 }
--- src/main/java/com/ganchurin/view/SimplePrintFormat.java
+++ src/main/java/com/ganchurin/view/SimplePrintFormat.java
@@ -1,6 +1,8 @@
 package com.ganchurin.view;
 
-public class SimplePrintFormat extends PrintFormat {
+import com.ganchurin.model.DataSource;
+
+public class SimplePrintFormat implements PrintFormat {
 
 	private final static char DefaultCornerChar = '+';
 	private final static char DefaultRowChar = '-';
@@ -47,5 +49,44 @@ public class SimplePrintFormat extends PrintFormat {
 	@Override
 	public SimplePrintFormat setColumnChar(char c) {
 		return new SimplePrintFormat(this.cornerChar, this.rowChar, c);
+	}
+
+	@Override
+	public String format(DataSource source) {
+		StringBuilder sb = new StringBuilder();
+
+		int rows = source.getRowsCount() * 2 + 1;
+		int cols = source.getColumnsCount() * 2 + 1;
+
+		for (int row = 0; row < rows; row++) {
+			if (!isDataRow(row)) {
+				for (int j = 0; j < cols; j++) {
+					if (!isDataColumn(j)) {
+						sb.append(getCornerChar());
+					} else {
+						sb.append(getRowChar());
+					}
+				}
+				sb.append("\n");
+				continue;
+			}
+			for (int col = 0; col < cols; col++) {
+				if (isDataColumn(col)) {
+					sb.append(source.getValue(row / 2, col / 2));
+				} else {
+					sb.append(getColumnChar());
+				}
+			}
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
+
+	private boolean isDataRow(int row) {
+		return row % 2 == 1;
+	}
+
+	private boolean isDataColumn(int col) {
+		return col % 2 == 1;
 	}
 }
